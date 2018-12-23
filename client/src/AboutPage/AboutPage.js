3f615896b3ac183d395f81939a59d2b85db5e00f
kgmsoft <princehome0505@gmail.com>
2018-12-23 16:12:59 +0100

    add Authorization based info to About page

--- client/src/AboutPage/AboutPage.js
+++ client/src/AboutPage/AboutPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { LoginOutButton, NotLoggedInAuthorization, LoggedInAuthorization } from '../common/Authorization'
+import { Link } from 'react-router-dom'
 
 class AboutPage extends React.Component {
   render () {
@@ -6,11 +8,21 @@ class AboutPage extends React.Component {
       <div className='section'>
         <h1 className='title'>About</h1>
         <p>
-          This is a simple app with authentication.
+          This is a simple app with <strong>Auth0 authentication.</strong>
         </p>
-        <p>
-          One page is hidden behind authentication.
-        </p>
+
+        <NotLoggedInAuthorization>
+          <p>
+            Some of the pages are visible after you<br />
+            <LoginOutButton className='hero-buttons' />
+          </p>
+        </NotLoggedInAuthorization>
+
+        <LoggedInAuthorization>
+          <p>
+            <strong>Great!</strong> You are logged in. Check your <Link to='/profile'>profile!</Link>
+          </p>
+        </LoggedInAuthorization>
       </div>
     )
   }
