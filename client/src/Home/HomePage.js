65d7dbc17fd64f96d8593e461e6c1a33faec3b6a
kgmsoft <princehome0505@gmail.com>
2018-12-23 16:13:27 +0100

    add Authorization based info to HomePage

--- client/src/Home/HomePage.js
+++ client/src/Home/HomePage.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import { NotLoggedInAuthorization, SalesRepresentativeAuthorization, BasicAuthorization } from '../common/Authorization'
+import { Link } from 'react-router-dom'
 
 class HomePage extends React.Component {
   render () {
     return (
       <div className='section'>
         <h1 className='title'>Home page</h1>
-        <p>
-          Please log in as a <b>sales representative</b> to reveal the main part.
-        </p>
+
+        <NotLoggedInAuthorization>
+          <p>
+            Please <strong>log in</strong> to reveal the app.
+          </p>
+        </NotLoggedInAuthorization>
+
+        <SalesRepresentativeAuthorization>
+          <p>
+            <strong>Great!</strong>{' '}
+            You are a sales representative, you can check customers{' '}
+            <Link to={'/customer-service'}>here.</Link>
+          </p>
+          <p>
+            You can check your <Link to={'/profile'}>profile</Link>.
+          </p>
+        </SalesRepresentativeAuthorization>
+
+        <BasicAuthorization>
+          <p>
+            <strong>Hi!</strong>{' '}
+            You're a user with basic settings, you can check your <Link to={'/profile'}>profile</Link>.
+          </p>
+        </BasicAuthorization>
       </div>
     )
   }
