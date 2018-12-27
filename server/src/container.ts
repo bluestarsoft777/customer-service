8ab90dc1f2dbccdfde1bd1383b80c122ac89b620
kgmsoft <princehome0505@gmail.com>
2018-12-27 11:54:05 +0100

    remove unused repo

--- server/src/container.ts
+++ server/src/container.ts
@@ -1,15 +1,6 @@
-import { userRepository } from './interfaces/storage/userRepository'
 import { customerRepository } from './interfaces/storage/customerRepository'
-// import { makeGetUserProfile } from './app/getUserProfile'
-// import { makeLoginUser } from './app/loginUser';
-// import { makeRegisterUser } from './app/registerUser'
 import { makeGetCustomerList } from './app/getCustomerList'
 import { makeGetCustomer } from './app/getCustomer'
-import { emailService } from './infrastructure/email'
-
-// const getUserProfile = makeGetUserProfile({ userRepository })
-// const loginUser = makeLoginUser({ userRepository })
-// const registerUser = makeRegisterUser({ userRepository, emailService })
 
 const getCustomerList = makeGetCustomerList({customerRepository})
 const getCustomer = makeGetCustomer({customerRepository})
@@ -18,8 +9,6 @@ const getCustomer = makeGetCustomer({customerRepository})
 export const container = {
   getCustomer,
   getCustomerList
-  // loginUser,
-  // registerUser
 }
 
 export type Container = typeof container
